203c17ccaba6ad3e421c0ddc1b4c07f3d51a9c32
hydrox19 <hydrox19@yandex.com>
2022-01-12 20:01:52 +0530

    fix(typings): finally

---
+++ src/typings/index.ts
@@ -0,0 +1,13 @@
+export interface SpotifyOptions {
+    clientId: string;
+    clientSecret: string;
+}
+
+export interface UnresolvedTrack {
+    artists: string[];
+    title: string;
+    duration: number;
+    originURL: string;
+    isResolved: boolean;
+}
+
