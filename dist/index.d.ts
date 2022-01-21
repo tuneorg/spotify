8505a024c96079a039eb5427c67ec9b60b2f0d5e
Hazmi35 <contact@hzmi.xyz>
2022-01-21 15:54:21 +0700

    ci: Don't publish dist

--- dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-export * from "./structures/Spotify";
-export * from "./typings";
--- dist/methods/Playlist.d.ts
+++
@@ -1,7 +0,0 @@
-import { Spotify } from "../structures/Spotify";
-import { UnresolvedData } from "../typings";
-export declare class Playlist {
-    private readonly spotify;
-    constructor(spotify: Spotify);
-    resolve(Id: string): Promise<UnresolvedData>;
-}
