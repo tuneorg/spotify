8fcda1a01a4e06119f2178fbff4de2039ae04575
hydrox19 <hydrox19@yandex.com>
2022-04-23 08:06:52 +0530

    fix(Track): fix the artist output

--- src/structures/SpotifyTrack.ts
+++ src/structures/SpotifyTrack.ts
@@ -12,8 +12,11 @@ export class SpotifyTrack {
         this.title = apiTrack.name;
         this.originURL = apiTrack.external_urls.spotify;
         this.duration = apiTrack.duration_ms;
-        // TODO: Fix the output
-        this.artists = apiTrack.artists;
+        this.artists = apiTrack.artists.map(artist => ({
+            name: artist.name,
+            id: artist.id,
+            url: artist.external_urls.spotify
+        }));
         this.isrc = apiTrack.external_ids?.isrc ?? undefined;
         this.thumbnail = apiTrack.album?.images[0].url ?? undefined;
     }
