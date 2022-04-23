677124ba950e80b54ec6e069929dbeb58031e61b
hydrox19 <hydrox19@yandex.com>
2022-04-23 07:50:35 +0530

    fix(endpoints): remove additional braces

--- src/constants/endpoints.ts
+++ src/constants/endpoints.ts
@@ -1,4 +1,4 @@
 export const track = (trackId: string): string => `/v1/tracks/${trackId}`;
 export const album = (albumId: string): string => `/v1/albums/${albumId}`;
-export const playlist = (playlistId: string): string => `/v1/playlists/${playlistId}}`;
+export const playlist = (playlistId: string): string => `/v1/playlists/${playlistId}`;
 export const artist = (artistId: string): string => `/v1/artists/${artistId}`;
